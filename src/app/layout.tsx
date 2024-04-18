22fcb7b87fdc3eeae8c2a092763a217bf9ba564b
Ivan Aquino <salinas.aquino.omarivan@gmail.com>
2024-04-18 05:45:15 -0500

    :C

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Epilogue } from "next/font/google";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {};
+const epilogue = Epilogue({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -12,7 +14,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>
+      <body
+        className={
+          `${epilogue.className} ${process.env.NODE_ENV == "development"}`
+            ? "debug-screens"
+            : ""
+        }
+      >
         <Providers>
           <Navbar />
           {children}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import logo from "@/assets/images/logo.svg";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 {
   /*Icons imports*/
 }
@@ -11,6 +12,9 @@ import { IoIosArrowDown } from "react-icons/io";
 import { GiCow } from "react-icons/gi";
 import { GiPlantSeed } from "react-icons/gi";
 import { BsLightningChargeFill } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import { Single_Day } from "next/font/google";
 
 type NavItem = {
   label?: string;
@@ -42,22 +46,34 @@ const navItems: NavItem[] = [
     ],
   },
   {
-    label: "Acerca de",
+    label: "Configuración",
     link: "#",
     children: [
       {
-        label: "¿Qué es Adgamus?",
+        label: "ChatBot",
         link: "#",
       },
       {
-        label: "Sobre nosotros",
+        label: "Comunidad",
         link: "#",
       },
       {
         label: "Novedades",
         link: "#",
       },
+      {
+        label: "Politicas de privacidad",
+        link: "#",
+      },
+      {
+        label: "¡Contáctanos!",
+        link: "#",
+      },
     ],
+  },
+  {
+    label: "Acerca de",
+    link: "#",
   },
 ];
 
@@ -81,7 +97,10 @@ function Navbar() {
           {/*Logo*/}
 
           <Image src={logo} alt="logo" />
-          <div className="flex items-center gap-4 transition-all">
+
+          <MobileNav />
+
+          <div className="hidden md:flex items-center gap-4 transition-all">
             {navItems.map((d, i) => (
               <Link
                 key={i}
@@ -90,7 +109,9 @@ function Navbar() {
               >
                 <p className="flex cursor-pointer items-center gap-2 text-elementos group-hover:text-resaltar">
                   <span>{d.label}</span>
-                  <IoIosArrowDown className="rotate-180 transition-all group-hover:rotate-0" />
+                  {d.children && (
+                    <IoIosArrowDown className="rotate-180 transition-all group-hover:rotate-0" />
+                  )}
                 </p>
 
                 {/*dropdown*/}
@@ -122,7 +143,7 @@ function Navbar() {
         </section>
         {/*seccion derecha*/}
 
-        <section className="flex items-center gap-8">
+        <section className="hidden md:flex items-center gap-8">
           <Link
             className="h-fit text-elementos transition-all hover:text-resaltar"
             href="/login"
@@ -136,6 +157,7 @@ function Navbar() {
             Registrar
           </Link>
         </section>
+        <GiHamburgerMenu className="cursor-pointer text-4xl md:hidden" />
 
         {/*
         <Link href="/">
@@ -163,6 +185,80 @@ function Navbar() {
 */}
       </div>
     </nav>
+  );
+}
+
+function MobileNav() {
+  return (
+    <div
+      className="fixed left-0 top-0 flex h-full min-h-screen w-full justify-end
+     bg-black/60 md:hidden"
+    >
+      <div className="h-full w-[65%] bg-white px-4 py-4">
+        <section className="flex justify-end">
+          <IoClose className="cursor-pointer text-4xl" />
+        </section>
+        <div className="flex flex-col text-base gap-4 transition-all">
+          {navItems.map((d, i) => (
+            <SingleNavItem
+              key={i}
+              label={d.label}
+              iconImage={d.iconImage}
+              link={d.link}
+            >
+              {d.children}
+            </SingleNavItem>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+function SingleNavItem(d: NavItem) {
+  const [animationParent] = useAutoAnimate();
+  const [isItemOpen, setItem] = useState(false);
+
+  function toggleItem() {
+    return setItem(!isItemOpen);
+  }
+
+  return (
+    <Link
+      ref={animationParent}
+      onClick={toggleItem}
+      href={d.link ?? "#"}
+      className="relative px-2 py-2 transition all"
+    >
+      <p className="flex cursor-pointer items-center gap-2 text-elementos group-hover:text-resaltar">
+        <span>{d.label}</span>
+        {d.children && (
+          <IoIosArrowDown
+            className={`text-xs transition-all ${isItemOpen && "rotate-180"}`}
+          />
+        )}
+      </p>
+
+      {/*dropdown*/}
+      {isItemOpen && d.children && (
+        <div
+          className="w-auto flex-col gap-1 rounded-lg bg-white 
+  py-3 transition-all flex"
+        >
+          {d.children.map((ch, i) => (
+            <Link
+              key={i}
+              href={ch.link ?? "#"}
+              className="flex cursor-pointer items-center py-1 pl-6 pr-8 text-neutral-400 hover:text-resaltar"
+            >
+              {/*image <Image src={} alt="itwm-icon" /> */}
+              {ch.iconImage}
+              {/*item*/}
+              <span className="whitespace-nowrap pl-3">{ch.label}</span>
+            </Link>
+          ))}
+        </div>
+      )}
+    </Link>
   );
 }
 
